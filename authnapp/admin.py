c1662b27a6b209405d48e2300dca00a45c235597
halexx7 <halexx7@gmail.com>
2021-04-13 15:38:21 +0300

    LK. Add Custom Model User

--- authnapp/admin.py
+++ authnapp/admin.py
@@ -1,3 +1,86 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-# Register your models here.
+from authnapp.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    """Форма для создания новых пользователей. Включает все обязательные поля,
+        а также повторный пароль."""
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('personal_account', 'email')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Пароли не совпадают")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """Форма для обновления пользователей. Включает все поля пользователя,
+    но заменяет поле пароля полем отображения хэша пароля администратора.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('__all__')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('personal_account', 'name')
+    # list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('personal_account', 'password')}),
+        ('Personal info', {'fields': ('name','email', 'phone')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('personal_account', 'email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('personal_account', 'name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.unregister(User)
+# Now register the new UserAdmin...
+admin.site.register(User, UserAdmin)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+# admin.site.unregister(Group)
--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,27 +1,46 @@
 from django.contrib import admin
 from django.db.models.base import ModelState
+from authnapp.models import User
 
-import mainapp.models
 
-admin.site.register(mainapp.models.ServicesCategory)
-admin.site.register(mainapp.models.Services)
-admin.site.register(mainapp.models.Metrics)
-admin.site.register(mainapp.models.City)
-admin.site.register(mainapp.models.Street)
-admin.site.register(mainapp.models.UK)
-admin.site.register(mainapp.models.House)
-admin.site.register(mainapp.models.HouseCurrent)
-admin.site.register(mainapp.models.HouseHistory)
-admin.site.register(mainapp.models.Appartament)
-admin.site.register(mainapp.models.User)
-admin.site.register(mainapp.models.UserProfile)
-admin.site.register(mainapp.models.CurrentCounter)
-admin.site.register(mainapp.models.HistoryCounter)
-admin.site.register(mainapp.models.ConstantPayments)
-admin.site.register(mainapp.models.VariablePayments)
-admin.site.register(mainapp.models.Subsidies)
-admin.site.register(mainapp.models.Privileges)
-admin.site.register(mainapp.models.Profit)
-admin.site.register(mainapp.models.Payment)
-admin.site.register(mainapp.models.Recalculations)
-admin.site.register(mainapp.models.Standart)
+from mainapp.models import ServicesCategory, Services, Metrics, City, Street, UK, House 
+from mainapp.models import HouseCurrent, HouseHistory, Appartament, UserProfile, CurrentCounter, HistoryCounter
+from mainapp.models import ConstantPayments, VariablePayments, Subsidies, Privileges, Profit, Payment, Recalculations, Standart
+
+class CommentInline(admin.TabularInline):
+    model = Services
+
+
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'updated')
+    search_fields = ['name',]
+    list_filter = ('created','updated',)
+    inlines = [CommentInline,]
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'updated')
+    search_fields = ['name',]
+    list_filter = ('category', 'created','updated',)
+    # inlines = [CommentInline,]
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('city','updated')
+    search_fields = ['city',]
+    list_filter = ('created','updated',)
+
+
+
+admin.site.register(ServicesCategory, ServiceCategoryAdmin)
+admin.site.register(Services, ServiceAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Metrics)
+admin.site.register(Street)
+admin.site.register(UK)
+admin.site.register(House)
+admin.site.register(HouseCurrent)
+admin.site.register(HouseHistory)
+admin.site.register(User)
+# admin.site.register(Appartament, UserProfile, CurrentCounter, HistoryCounter, ConstantPayments, VariablePayments)
+# admin.site.register(Subsidies, Privileges, Profit, Payment, Recalculations, Standart)
