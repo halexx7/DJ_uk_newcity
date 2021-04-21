9495a5207129a85e6abf841ccca7aa4a1cd0e6f6
Aleksey Khlybov <halexx7@gmail.com>
2021-04-21 06:56:19 +0300

    LK. Reset passwd

--- authnapp/urls.py
+++ authnapp/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     re_path(r"^register/$", authnapp.register, name="register"),
     re_path(r"^edit/$", authnapp.edit, name="edit"),
     re_path(r"^verify/(?P<email>.+)/(?P<activation_key>\w+)/$", authnapp.verify, name="verify"),
+#     re_path(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+#     re_path(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+#     re_path(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
+#     re_path(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
 ]
