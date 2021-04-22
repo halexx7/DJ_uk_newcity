08f33265c79187e723007498fea8b34195ebe393
Aleksey Khlybov <halexx7@gmail.com>
2021-04-22 06:22:29 +0300

    LK. Reset pwd step_1

--- authnapp/urls.py
+++ authnapp/urls.py
@@ -4,7 +4,7 @@ from django.urls.base import reverse, reverse_lazy
 import authnapp.views as authnapp
 from authnapp.apps import AuthnappConfig
 
-from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView, PasswordResetView
 from .forms import MyPasswordChangeForm
 
 app_name = AuthnappConfig.name
@@ -27,7 +27,11 @@ urlpatterns = [
         template_name='authnapp/password_change_done.html',
     ), name='password_change_done'),
 
-#     re_path(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    # reset password urls
+
+    re_path(r"^password-reset/$", PasswordResetView.as_view(
+        template_name='authnapp/password_reset.html',
+    ), name='password_reset'),
 #     re_path(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
 #     re_path(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
 #     re_path(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
