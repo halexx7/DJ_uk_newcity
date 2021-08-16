584270a81686b53df2b740a09808baa3ecf5b826
halexx7 <63438275+halexx7@users.noreply.github.com>
2021-08-17 00:48:49 +0300

    Django SOLO (#11)
    * Upd pipfile
    
    * Django solo + ya map
    
    * Added migrations
    
    * Added dumps db

--- newcity/settings.py
+++ newcity/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "directory",
     "invoice",
+    "solo",
 ]
 
 # Auth model
--- personalacc/admin.py
+++ personalacc/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from solo.admin import SingletonModelAdmin
+from .models import SiteConfiguration
 
-# Register your models here.
+admin.site.register(SiteConfiguration, SingletonModelAdmin)
