b58b8f8171738164d9893e1de62cbe1f0f9cc6d6
Alexey Khlybov <halexx7@gmail.com>
2021-08-24 23:22:32 +0000

    VALID-2 Валидация модели Юзера

--- authnapp/models.py
+++ authnapp/models.py
@@ -27,11 +27,11 @@ class User(AbstractBaseUser, PermissionsMixin):
                                         unique=True, help_text="Введите номер лицевого счета: 777777",
                                         validators=[
                                             RegexValidator(r'\d',
-                                            "Телефон состоит только из цифр!"),
-                                            MinLengthValidator(10,
-                                            "Телефон состоит из 11 цифр. Вы ввели меньше."),
-                                            MaxLengthValidator(12,
-                                            "Телефон состоит из 11 цифр. Вы ввели больше.")])
+                                            "Лицевой счет состоит только из цифр!"),
+                                            MinLengthValidator(6,
+                                            "Лицевой счет состоит из 6 цифр. Вы ввели меньше."),
+                                            MaxLengthValidator(6,
+                                            "Лицевой счет состоит из 6 цифр. Вы ввели больше.")])
     password = models.CharField(verbose_name="Пароль", max_length=128)
     name = models.CharField(verbose_name="ФИО", max_length=128, null=True,
                             blank=True, help_text="Иванов Иван Иванович",
@@ -47,11 +47,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     phone = models.CharField(verbose_name="Телефон", max_length=11, null=True, blank=True, 
                              help_text="Номер телефона в формате - 79823212334",
                              validators=[
-                                 RegexValidator(r'\d',
+                                 RegexValidator(r'\d{6}',
                                  "Телефон состоит только из цифр!"),
-                                 MinLengthValidator(10,
+                                 MinLengthValidator(11,
                                  "Телефон состоит из 11 цифр. Вы ввели меньше."),
-                                 MaxLengthValidator(12,
+                                 MaxLengthValidator(11,
                                  "Телефон состоит из 11 цифр. Вы ввели больше.")])
     is_active = models.BooleanField(verbose_name="Активный", default=True)
     is_superuser = models.BooleanField(default=False, verbose_name="Суперпользователь",
@@ -73,28 +73,18 @@ class User(AbstractBaseUser, PermissionsMixin):
         return f"({self.personal_account}) - {self.name}"
 
     def get_full_name(self):
-        """
-        Returns the name plus the last_name plus patronymic, with a space in between.
-        """
         full_name = f"{self.name}"
         return full_name.strip()
 
     def get_short_name(self):
-        """
-        Returns the short name for the user.
-        """
         return self.name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
-        """
-        Sends an email to this User.
-        """
+        """Send an email to this User"""
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
     def is_activation_key_expired(self):
-        """
-        Checks if the key is up-to-date.
-        """
+        """Checks if the key is up-to-date."""
         if now() <= self.activation_key_expires:
             return False
         else:
