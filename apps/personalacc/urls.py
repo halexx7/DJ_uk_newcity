f08b297c659f58b55b3cfd01658b21a651d67c72
Хлыбов Алексей Григорьевич <AGKhlybov@sberbank.ru>
2022-01-28 11:50:11 +0300

    Теперь автокомплит работает как на цифры так и буквы

--- apps/personalacc/urls.py
+++ apps/personalacc/urls.py
@@ -7,9 +7,13 @@ app_name = PersonalaccConfig.name
 
 urlpatterns = [
     re_path(r"^user/$", personalacc.UserPageCreate.as_view(), name="user"),
-    re_path(r"^manager/dal_user/$", personalacc.UserAutocomplete.as_view(), name="dal_user"),
     re_path(r"^manager/$", personalacc.ManagerPageCreate.as_view(), name="manager"),
+    # Autocomplete
+    re_path(r"^manager/dal_user/$", personalacc.UserAutocomplete.as_view(), name="dal_user"),
+    
     re_path(r"^manager/formation_payments/$", personalacc.FormationPayments.as_view(), name="formation_payments"),
+    re_path(r"^manager/receivable/$", personalacc.AccountsReceivableListView.as_view(), name="receivable"),
+    
     re_path(r"^manager/house_history/$", personalacc.HouseHistoryListView.as_view(), name="house_history"),
     re_path(r"^manager/recalc_history/$", personalacc.RecalcHistoryListView.as_view(), name="recalc_history"),
     re_path(
@@ -17,5 +21,4 @@ urlpatterns = [
     ),
     re_path(r"^manager/subsidies_history/$", personalacc.SubsidiesHistoryListView.as_view(), name="subsidies_history"),
     re_path(r"^manager/payments_history/$", personalacc.PaymentsHistoryListView.as_view(), name="payments_history"),
-    re_path(r"^manager/receivable/$", personalacc.AccountsReceivableListView.as_view(), name="receivable"),
 ]
--- apps/personalacc/views.py
+++ apps/personalacc/views.py
@@ -275,7 +275,9 @@ class UserAutocomplete(autocomplete.Select2QuerySetView):
         if not self.request.user.is_authenticated:
             return User.objects.none()
         qs = User.objects.filter(is_staff=False)
-        if self.q:
+        if self.q.isdigit():
+            qs = qs.filter(personal_account__istartswith=self.q)
+        if self.q.isalpha():
             qs = qs.filter(name__istartswith=self.q)
         return qs
 
