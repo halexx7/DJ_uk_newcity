b6d4797509f7a3a48047bca49c4ba1cc82d9a916
Хлыбов А.Г <AGKhlybov@sberbank.ru>
2022-06-11 12:20:31 +0300

    Исправление автокомплете

--- static/js/paccount/autocomplete.js
+++ static/js/paccount/autocomplete.js
@@ -1,6 +1,6 @@
 async function autocomplete(inp) {
-    let currentFocus, resp;
     let arr = [];
+    let currentFocus, a, b, i, body, val = inp.value;
     let request = {
         url: `${window.location.href}autocomplete/`,
         csrf: getCookieCSRF(),
@@ -80,82 +80,78 @@ async function autocomplete(inp) {
 
     // Асинхронный запрос на сервер
     async function getAjaxRequest(body) {
-        const response = await fetch(request.url, {
+        const responce = await fetch(request.url, {
             method: 'POST',
             headers: {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'X-CSRFToken': request['csrf']
             },
-            body: JSON.stringify(body)})
-        
-        if (response.ok) {
-            let json = await response.json();
+            body: JSON.stringify(body)
+        })
+
+        if (responce.ok) {
+            let json = await responce.json();
             return json;
         }
     };
 
-    inp.addEventListener("input", async function(e) {
-        let a, b, i, body, val = this.value;
-        // closeAllLists();
-
-        // Проверяем добавлен ли уже елемент - Выпадающий список
-        a = document.getElementById(`${this.id}-autocomplete-list`);
-        if (!a) {
-            // Если нет, то создаем
-            a = document.createElement("DIV");
-            a.setAttribute("id", this.id + "-autocomplete-list");
-            a.setAttribute("class", "autocomplete-items");
-        } else {
-            // если да, то чистим
-            a.innerHTML = '';
+    // Проверяем добавлен ли уже элемент - Выпадающий список
+    a = document.getElementById(`${inp.id}-autocomplete-list`);
+    if (!a) {
+        // Если нет, то создаем
+        a = document.createElement("DIV");
+        a.setAttribute("id", inp.id + "-autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        inp.parentNode.appendChild(a);
+    } else {
+        // если да, то чистим
+        // a.innerHTML = '';
+        while (a.firstChild) {
+            a.removeChild(a.firstChild);
         }
+    }
 
-        if (!val || (val.length < 3)) { return false;}
-        currentFocus = -1;
+    // Если короче 3 символов, ничего не делаем
+    if (!val || (val.length < 3) || !a) { return false;}
+    currentFocus = -1;
 
-        body = {type: this.id, value: this.value}
-        resp = await getAjaxRequest(body);
+    body = {type: inp.id, value: inp.value}
+    const responce = await getAjaxRequest(body);
 
-        arr = []
-        for (let [key, value] of Object.entries(resp.value)) {
-            arr.push(`${key} - ${value}`);
-        }
+    // arr = []
+    for (let [key, value] of Object.entries(responce.value)) {
+        arr.push(`${key} - ${value}`);
+    }
 
-        // a = document.createElement("DIV");
-        // a.setAttribute("id", this.id + "-autocomplete-list");
-        // a.setAttribute("class", "autocomplete-items");
-
-        this.parentNode.appendChild(a);
-        for (i = 0; i < arr.length; i++) {
-          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
             b = document.createElement("DIV");
             b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
             b.innerHTML += arr[i].substr(val.length);
             b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
 
-                b.addEventListener("click", function(e) {
-                inp.value = this.getElementsByTagName("input")[0].value;
-                // closeAllLists();
+            b.addEventListener("click", function(e) {
+                inp.value = e.target.textContent;
+                closeAllLists(a);
             });
             a.appendChild(b);
-          }
         }
-    });
+    };
 
     inp.addEventListener("keydown", function(e) {
         var x = document.getElementById(this.id + "autocomplete-list");
         if (x) x = x.getElementsByTagName("div");
         if (e.keyCode == 40) {
-          currentFocus++;
-          addActive(x);
+            currentFocus++;
+            addActive(x);
         } else if (e.keyCode == 38) { //up
-          currentFocus--;
-          addActive(x);
+            currentFocus--;
+            addActive(x);
         } else if (e.keyCode == 13) {
-          e.preventDefault();
-          if (currentFocus > -1) {
-            if (x) x[currentFocus].click();
-          }
+            e.preventDefault();
+            if (currentFocus > -1) {
+                if (x) x[currentFocus].click();
+            }
         }
     });
 
@@ -168,59 +164,58 @@ async function autocomplete(inp) {
     }
 
     function addActive(x) {
-      if (!x) return false;
-      removeActive(x);
-      if (currentFocus >= x.length) currentFocus = 0;
-      if (currentFocus < 0) currentFocus = (x.length - 1);
-      x[currentFocus].classList.add("autocomplete-active");
+        if (!x) return false;
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        x[currentFocus].classList.add("autocomplete-active");
     }
 
     function removeActive(x) {
-      for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("autocomplete-active");
-      }
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
     }
 
-    function closeAllLists(elmnt) {
-      var x = document.getElementsByClassName("autocomplete-items");
-      for (var i = 0; i < x.length; i++) {
-        if (elmnt != x[i] && elmnt == inp) {
-        x[i].parentNode.removeChild(x[i]);
-      }
-    }
-    }
+    function closeAllLists(a) {
+        const els = Array.prototype.slice.call(a.childNodes);
+        els.forEach(function(elem, idx) {
+            elem.parentNode.removeChild(elem);
+          });
+        }
 
     document.addEventListener("click", function (e) {
-        closeAllLists(e.target);
+        closeAllLists();
     });
-
-    //Ловим событие формы PAYMENTS
-    document.querySelector("#paymentsBtn").addEventListener('click', event => {
-        let formID = 'payments_form';
-        let reDrawDiv = 'paymentsList';
-        saveDataForm(event, formID, reDrawDiv).then()});
-
-    // //Ловим событие формы CURRENT
-    // document.querySelector("#currentCountBtn").addEventListener('click', event => {
-    //     let formID = 'house_count_form';
-    //     let reDrawDiv = '.house-current_list';
-    //     saveDataForm(event, formID, reDrawDiv).then()})
-
-    //Ловим событие формы RECALCULATIONS
-    document.getElementById("recalcBtn").addEventListener('click', event => {
-        let formID = 'recalculations_form';
-        let reDrawDiv = 'recalcList';
-        saveDataForm(event, formID, reDrawDiv).then()})
-
-    //Ловим событие формы PRIVILEGE
-    document.getElementById("privilegeBtn").addEventListener('click', event => {
-        let formID = 'privilege_form';
-        let reDrawDiv = 'privilegesList';
-        saveDataForm(event, formID, reDrawDiv).then()})
-
-    // //Ловим событие формы SUBSIDIES
-    // document.querySelector("#subsidiesBtn").addEventListener('click', event => {
-    //     let formID = 'subsidies_form';
-    //     let reDrawDiv = '.subsidies_list';
-    //     saveDataForm(event, formID, reDrawDiv).then()})
 }
+
+
+//Ловим событие формы PAYMENTS
+document.getElementById("paymentsBtn").addEventListener('click', event => {
+    let formID = 'payments_form';
+    let reDrawDiv = 'paymentsList';
+    saveDataForm(event, formID, reDrawDiv).then()});
+
+// //Ловим событие формы CURRENT
+// document.querySelector("#currentCountBtn").addEventListener('click', event => {
+//     let formID = 'house_count_form';
+//     let reDrawDiv = '.house-current_list';
+//     saveDataForm(event, formID, reDrawDiv).then()})
+
+// //Ловим событие формы RECALCULATIONS
+// document.getElementById("recalcBtn").addEventListener('click', event => {
+//     let formID = 'recalculations_form';
+//     let reDrawDiv = 'recalcList';
+//     saveDataForm(event, formID, reDrawDiv).then()})
+
+// //Ловим событие формы PRIVILEGE
+// document.getElementById("privilegeBtn").addEventListener('click', event => {
+//     let formID = 'privilege_form';
+//     let reDrawDiv = 'privilegesList';
+//     saveDataForm(event, formID, reDrawDiv).then()})
+
+// //Ловим событие формы SUBSIDIES
+// document.querySelector("#subsidiesBtn").addEventListener('click', event => {
+//     let formID = 'subsidies_form';
+//     let reDrawDiv = '.subsidies_list';
+//     saveDataForm(event, formID, reDrawDiv).then()})
